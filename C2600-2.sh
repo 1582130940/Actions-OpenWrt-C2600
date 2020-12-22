8b4c7f17eb9eb2e797af7c409da6ef5290c7a5ee
1582130940 <1582130940@qq.com>
2020-12-22 19:14:26 +0800

    C2600-2: Set LuCI Default Theme to Material

--- C2600-2.sh
+++ C2600-2.sh
@@ -6,3 +6,6 @@
 
 # Add Thermal Status (By YYiiEt)
 sed -i 's/or "1"%>/or "1"%> ( <%=luci.sys.exec("expr `cat \/sys\/class\/thermal\/thermal_zone0\/temp` \/ 1000") or "?"%> \&#8451; ) /g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
+
+# Set LuCI Default Theme to Material
+sed -i 's/luci-theme-bootstrap/luci-theme-material/g' feeds/luci/collections/luci/Makefile
