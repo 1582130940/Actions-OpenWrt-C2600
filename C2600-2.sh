58612789a2857b588d3348586de35926a45704bb
1582130940 <1582130940@qq.com>
2020-12-22 09:52:40 +0800

    C2600: Initial commit

---
+++ C2600-2.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# Description: TP-Link Archer C2600 Source Modify
+# Author: 1582130940
+# Github: https://github.com/1582130940
+
+# Add Thermal Status (By YYiiEt)
+sed -i 's/or "1"%>/or "1"%> ( <%=luci.sys.exec("expr `cat \/sys\/class\/thermal\/thermal_zone0\/temp` \/ 1000") or "?"%> \&#8451; ) /g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
